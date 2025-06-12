0d6309a84c498c11efada81f18e04707e8ba2c53
Juan Pablo <jsimon0301@gmail.com>
2025-06-11 23:53:08 -0300

    Conexión a base de datos de libros

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'mydb'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "mydb",
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error al conectar con la BD:', err);
-        return;
-    }
-    console.log('Conexión a MySQL exitosa');
+  if (err) {
+    console.error("Error al conectar con la BD:", err);
+    return;
+  }
+  console.log("Conexión a MySQL exitosa");
 });
 
 module.exports = connection;
