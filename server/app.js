2a96e359ecf19b264abce328bd8a58df69245b4d
Nicolás M. Perez <nicomati0514@gmail.com>
2025-06-11 23:43:09 -0300

    Añado cors al app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 const usuarioRoutes = require("./routes/usuarioRoutes");
 const libroRoutes = require("./routes/libroRoutes");
